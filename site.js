c0def305a42350a24e91abe3517263399cd18599
Zroogz <ruegzegger@gmail.com>
2026-02-11 17:12:36 -0600

    Added welcome div

---
+++ site.js
@@ -0,0 +1,27 @@
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+console.log(isAfternoon)
+
+const Welcome = document.getElementById('Welcome');
+
+if (isMorning) {
+    
+    Welcome.textContent = "Good Morning! Time to get going!"
+
+}
+
+if (isAfternoon) {
+    
+    Welcome.textContent = "Good Afternoon! Take a break!"
+
+}
+
+if (isEvening) {
+    
+    Welcome.textContent = "Good Evening! Don't work too late!"
+
+}
